fbab65a57d45d2e18b1411fb341c38ff5945eb33
rock <rock@localhost.localdomain>
2019-03-28 09:47:19 +0000

    fix install

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,20 +2,20 @@
 # so any connection to Postgresql keeps working without any other modification.
 # Requires the --privileged flag when creating container:
 
-. /.docker-config
+#. /.docker-config
 
 sysctl -w net.ipv4.conf.all.route_localnet=1
-iptables -t nat -A OUTPUT -p tcp -s 127.0.0.1 --dport $extport -j DNAT --to-destination 172.20.0.1:$inport
+iptables -t nat -A OUTPUT -p tcp -s 127.0.0.1 --dport 18080 -j DNAT --to-destination 172.20.0.1:80
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
 /etc/init.d/rcS
 
 # Wait 40 seconds to start monitoring
-sleep $launchdelay
+sleep 40
 
 # Method 1
 # busybox wait for process to exit
-while (ps | grep $waitproc | grep -v grep); do sleep 3; done
+while (ps | grep bootexec | grep -v grep); do sleep 3; done
 
 # Method 2
 #pid=`pidof $waitproc`
