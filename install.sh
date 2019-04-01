9cfe72d02bc48a12476bef4eb36f29995bf27199
Jason Fisher <jason.fisher@gmail.com>
2019-03-31 20:34:31 -0700

    update install instructions

--- install.sh
+++ install.sh
@@ -7,10 +7,15 @@
 ./build-docker-from-rootfs.sh
 
 # cleanup
-# sudo rm -rf ./docker/rootfs
-# sudo rm -rf ./rootfs-excluded
+sudo rm -rf ./docker/rootfs
+sudo rm -rf ./rootfs-excluded
 
 echo
 echo "Docker and $name installed.  Launch $name from your Applications menu."
+echo
+
+echo "To launch from a tty, logout and back in and then run $name-boot.sh"
+echo
+echo "To boot directly into $name: systemctl enable $name"
 echo
 
